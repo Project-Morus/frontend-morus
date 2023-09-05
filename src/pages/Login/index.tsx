0d825fcc2a0f8499e10b8af54eca647fe4bed8ab
larissadantier <larissa_dantier@hotmail.com>
2023-09-05 02:33:32 -0300

    feat: insert new component in Login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,13 +1,51 @@
-import { useTheme } from "styled-components";
+import Aside from "../../components/Aside";
+import Button from "../../components/Button";
+import Input from "../../components/Input";
+import {
+  ContentSC,
+  FooterSC,
+  ForgotPasswordSC,
+  FormSC,
+  TextSC,
+  TitleSC,
+  WithoutAccountSC,
+  WrapperButtonSC,
+  WrapperSC
+} from "./styles";
 
 const Login = () => {
-  const theme = useTheme();
+
+  const handleSubmit = () => { }
+
   return (
-    <div>
-      <h1 style={{ fontSize: `${theme.typography.HEADERS_SIZES.h1}` }}>
-        Login
-      </h1>
-    </div>
+    <main>
+      <Aside position="left">
+        <WrapperSC>
+
+          <ContentSC>
+            <TitleSC>
+              Bem vindo!
+            </TitleSC>
+
+            <TextSC>Seja parte ativa da vida condominial! Faça login para participar de enquetes, reservar espaços e colaborar para um ambiente ainda melhor!</TextSC>
+
+            <FormSC onSubmit={() => console.log('Enviou')}>
+              <Input id="e-mail" label="E-mail" placeholder="Insira o seu e-mail" />
+              <Input id="passaword" label="Senha" placeholder="Insira a sua senha" type="password" />
+
+              <ForgotPasswordSC>Esqueci a minha senha</ForgotPasswordSC>
+
+              <WrapperButtonSC>
+                <Button text="Entrar" maxWidth={370} />
+              </WrapperButtonSC>
+            </FormSC>
+
+            <WithoutAccountSC>Ainda não possui uma conta? <strong style={{ display: 'inline-block' }}>Cadastre-se aqui</strong></WithoutAccountSC>
+
+          </ContentSC>
+        </WrapperSC>
+      </Aside>
+    </main>
   );
 };
 
