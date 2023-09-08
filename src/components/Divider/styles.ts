0b1c7147bf05822b52a511d7f614c6c601ef9b00
Eduardo Angelo Simon <eduardosimon.trabalho@gmail.com>
2023-09-08 11:01:24 -0300

    style: adjusting divider width

--- src/components/Divider/styles.ts
+++ src/components/Divider/styles.ts
@@ -2,9 +2,9 @@ import { styled } from "styled-components";
 import { BASE_STYLE } from "./stylesUtils";
 
 export const DividerPrimarySC = styled(BASE_STYLE)`
-  border: ${({ theme }) => theme.border.width.bold} solid ${({ theme }) => theme.colors.primary[50]};
+  border: ${({ theme }) => theme.border.width.regular} solid ${({ theme }) => theme.colors.primary[50]};
 `;
 
 export const DividerSecondarySC = styled(BASE_STYLE)`
-  border: ${({ theme }) => theme.border.width.bold} solid ${({ theme }) => theme.colors.grey[75]};
+  border: ${({ theme }) => theme.border.width.regular} solid ${({ theme }) => theme.colors.grey[75]};
 `;
