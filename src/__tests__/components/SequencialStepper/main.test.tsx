e0e34ef2b25fc13ad9af3a623d1a79abb26f26a6
larissadantier <larissa_dantier@hotmail.com>
2023-09-11 17:59:47 -0300

    test: creating test and snapshot of component

---
+++ src/__tests__/components/SequencialStepper/main.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from '@testing-library/react'
+import { withTheme } from '../../../helpers/withTheme'
+import SequencialStepper from '../../../components/SequencialStepper'
+import { SEQUENTIAL_STEPPER_DATA } from './mockData'
+
+const SequencialStepperWithTheme = withTheme(SequencialStepper)
+
+describe('SequencialStepper component', () => {
+  test('Verify if component is rendering', () => {
+    render(<SequencialStepperWithTheme role='roleSequecialStepper' items={SEQUENTIAL_STEPPER_DATA} currentStep={1} />)
+
+    expect(screen.getByRole('roleSequecialStepper'))
+  })
+
+  test('Snapshot SequencialStepper', () => {
+    const { asFragment } = render(
+      <SequencialStepperWithTheme items={SEQUENTIAL_STEPPER_DATA} currentStep={1} />,
+    )
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  test('Snapshot SequencialStepper with currentStep in two', () => {
+    const { asFragment } = render(
+      <SequencialStepperWithTheme items={SEQUENTIAL_STEPPER_DATA} currentStep={2} />,
+    )
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  test('Snapshot SequencialStepper with currentStep in last step', () => {
+    const { asFragment } = render(
+      <SequencialStepperWithTheme items={SEQUENTIAL_STEPPER_DATA} currentStep={3} />,
+    )
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
---
+++ src/__tests__/components/SequencialStepper/mockData.ts
@@ -0,0 +1,13 @@
+import { ISequencialStepperProps } from "../../../components/SequencialStepper/types";
+
+export const SEQUENTIAL_STEPPER_DATA: ISequencialStepperProps['items'] = [
+  {
+    key: 1,
+  },
+  {
+    key: 2,
+  },
+  {
+    key: 3,
+  },
+]
