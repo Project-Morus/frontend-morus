7a54c58c56429347302b40477ed7a222b952056c
larissadantier <larissa_dantier@hotmail.com>
2023-09-12 18:47:06 -0300

    feat: adding example of RadioGroupButton

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,16 +1,22 @@
-import { SEQUENTIAL_STEPPER_DATA } from "../../__tests__/components/SequencialStepper/mockData";
-import SequencialStepper from "../../components/SequencialStepper";
+import { useState } from "react";
+import RadioButtonGroup from "../../components/RadioButtonGroup";
+import { RADIO_BUTTON_GROUP_DATA } from "../../__tests__/components/RadioButtonGroup/mockData";
 
 const LandingPage = () => {
+  const [selectedValue, setSelectedValue] = useState<string>(RADIO_BUTTON_GROUP_DATA[0].label);
+
+
+  function drinkSelectionHandler(event: React.ChangeEvent<HTMLInputElement>) {
+    setSelectedValue(event.target.value);
+  }
+
   return (
     <>
       <h1>Landing Page</h1>
 
-      <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-        <SequencialStepper items={SEQUENTIAL_STEPPER_DATA} currentStep={1} />
-        <SequencialStepper items={SEQUENTIAL_STEPPER_DATA} currentStep={2} />
-        <SequencialStepper items={SEQUENTIAL_STEPPER_DATA} currentStep={3} />
-      </div>
+      <RadioButtonGroup options={RADIO_BUTTON_GROUP_DATA} label="Bebidas" onChange={drinkSelectionHandler} />
+
+      <p style={{ marginTop: '1rem' }}>Radio selecionado: {selectedValue}</p>
     </>
   );
 }
