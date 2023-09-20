e81af6654992100a2b05077e017a528417eef4b4
larissadantier <larissa_dantier@hotmail.com>
2023-09-20 00:12:46 -0300

    feat: creating component table

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,8 +1,15 @@
+import { HEADER_TABLE_CELLS, ROW_NAME_CELLS } from "../../__tests__/components/Table/mockData";
+import Table from "../../components/Table";
+
 const LandingPage = () => {
   return (
-    <>
+    <div style={{ width: '100%', height: '100vh', background: '#E6EEF6' }}>
       <h1>Landing Page</h1>
-    </>
+
+
+      <Table headerCells={HEADER_TABLE_CELLS} rowCells={ROW_NAME_CELLS} />
+      <Table headerCells={HEADER_TABLE_CELLS} rowCells={[]} emptyMessage='A tabela está vazia no momento. Espere o síndico adicionar novas informações!' />
+    </div>
   );
 }
 
