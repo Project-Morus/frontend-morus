4c2e35396a5da1173e9cec695cc8e6e2ed86c4a7
Larissa Dantier <larissa_dantier@hotmail.com>
2023-09-20 21:38:38 -0300

    S6 [Task] Componente Tabela (#14)
    * feat: creating new component SequencialStepper
    
    * test: creating test and snapshot of component
    
    * feat: adding component in LandingPage for review
    
    * feat: creating component table
    
    * fix: fixing some things and create example in landing page

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,8 +1,16 @@
+import { HEADER_TABLE_CELLS, ROW_NAME_CELLS } from "../../__tests__/components/Table/mockData";
+import Table from "../../components/Table";
+
 const LandingPage = () => {
   return (
-    <>
+    <div style={{ width: '100%', height: '100vh', background: '#E6EEF6' }}>
       <h1>Landing Page</h1>
-    </>
+
+
+      <div style={{ height: '100vh', background: '#E6EEF6', padding: '1rem' }}>
+        <Table headerCells={HEADER_TABLE_CELLS} rowCells={ROW_NAME_CELLS} emptyMessage='A tabela está vazia no momento. Espere o síndico adicionar novas informações!' />
+      </div>
+    </div>
   );
 }
 
