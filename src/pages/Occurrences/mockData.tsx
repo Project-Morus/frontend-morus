bf8e525b29a1de7d95d507074189054ce1cd0cb6
larissadantier <larissa_dantier@hotmail.com>
2023-09-26 21:05:32 -0300

    fix: remove extra in mock

--- src/pages/Occurrences/mockData.tsx
+++ src/pages/Occurrences/mockData.tsx
@@ -5,12 +5,3 @@ export const HEADER_TABLE_CELLS: ITableHeaderProps[] = [
   { colName: "Data da Ocorrência" },
   { colName: "Status" },
 ];
-
-export const ROW_NAME_CELLS = [
-  { reason: "Carro bateu na pilastra da garagem", date: "01/11/2023", status: 'Resolvido' },
-  { reason: "Elevador quebrou devido à sobrecarga elétrica", date: "01/11/2023", status: 'Resolvido' },
-  { reason: "Bicicleta desapareceu do bicicletário", date: "01/11/2023", status: 'Pendente' },
-  { reason: "Vidro da portaria quebrou com o vento", date: "01/11/2023", status: 'Resolvido' },
-  { reason: "Esteira da academia parou de funcionar", date: "01/11/2023", status: 'Pendente' },
-  { reason: "Portão da garagem enguiçou", date: "01/11/2023", status: 'Pendente' },
-];
