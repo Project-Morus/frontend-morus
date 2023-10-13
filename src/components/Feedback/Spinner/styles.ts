24e13805aed0d699159ccd81a3c672882f7287ef
larissadantier <larissa_dantier@hotmail.com>
2023-10-13 18:47:55 -0300

    style: fix extrabold

--- src/components/Feedback/Spinner/styles.ts
+++ src/components/Feedback/Spinner/styles.ts
@@ -3,9 +3,9 @@ import styled from "styled-components";
 export const LoaderSC = styled.span`
    width: 20px;
     height: 20px;
-    border: 3px solid ${({ theme }) => theme.colors.white[50]};
+    border: ${({ theme }) => theme.border.width.extraBold} solid ${({ theme }) => theme.colors.white[50]};
     border-bottom-color: ${({ theme }) => theme.colors.primary[200]};
-    border-radius: 50%;
+    border-radius: ${({ theme }) => theme.border.radius.circle};
     display: inline-block;
     box-sizing: border-box;
     animation: rotation 1s linear infinite;
