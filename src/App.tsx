4c6c94d8532d7e36f303885804a4852159eba561
larissadantier <larissa_dantier@hotmail.com>
2023-10-13 14:32:47 -0300

    feat: adding QueryClienteProvider in Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styles/themes/default';
 import { GlobalStyle } from './styles/themes/global';
-import { BrowserRouter } from 'react-router-dom';
+
 import Router from './routes/Router';
 
+const queryClient = new QueryClient()
 
 function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <BrowserRouter>
+
+      <QueryClientProvider client={queryClient}>
         <Router />
-      </BrowserRouter>
+      </QueryClientProvider>
     </ThemeProvider>
   )
 }
