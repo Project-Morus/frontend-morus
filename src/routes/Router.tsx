9831abecfe8903b171398abcebc447590e49f0e6
larissadantier <larissa_dantier@hotmail.com>
2023-10-13 14:32:27 -0300

    feat: change BrowserRouter for archive router

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,32 +1,34 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import DefaultLayoutAuth from "../layout/DefaultLayoutAuth";
 import DefaultLayoutSystem from "../layout/DefaultLayoutSystem";
-import { Login, CondominiumRegister, ForgotPasswordFirstStep,  ForgotPasswordSecondStep,LandingPage, Home, Residents, Archives, CashBook, Occurrences, Fines } from "../pages";
+import { Login, CondominiumRegister, ForgotPasswordFirstStep, ForgotPasswordSecondStep, LandingPage, Home, Residents, Archives, CashBook, Occurrences, Fines } from "../pages";
 
 const Router = () => {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
 
-      <Route element={<DefaultLayoutAuth />}>
-        <Route path="login" element={<Login />} />
-        <Route path="condominium-register" element={<CondominiumRegister />} />
-      </Route>
+        <Route element={<DefaultLayoutAuth />}>
+          <Route path="login" element={<Login />} />
+          <Route path="condominium-register" element={<CondominiumRegister />} />
+        </Route>
 
-      <Route path="forgotPassword">
-        <Route path="sendEmail" element={<ForgotPasswordFirstStep />} />
-        <Route path="newPassword" element={<ForgotPasswordSecondStep />} />
-      </Route>
+        <Route path="forgotPassword">
+          <Route path="sendEmail" element={<ForgotPasswordFirstStep />} />
+          <Route path="newPassword" element={<ForgotPasswordSecondStep />} />
+        </Route>
 
-      <Route path="system" element={<DefaultLayoutSystem />}>
-        <Route path="" element={<Home />} />
-        <Route path="residents" element={<Residents />} />
-        <Route path="fines" element={<Fines />} />
-        <Route path="archives" element={<Archives />} />
-        <Route path="cashBook" element={<CashBook />} />
-        <Route path="occurrences" element={<Occurrences />} />
-      </Route>
-    </Routes>
+        <Route path="system" element={<DefaultLayoutSystem />}>
+          <Route path="" element={<Home />} />
+          <Route path="residents" element={<Residents />} />
+          <Route path="fines" element={<Fines />} />
+          <Route path="archives" element={<Archives />} />
+          <Route path="cashBook" element={<CashBook />} />
+          <Route path="occurrences" element={<Occurrences />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
