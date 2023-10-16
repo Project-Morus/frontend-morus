362fb597ef26c5fb7a6138b2ab08f4bc360886c5
larissadantier <larissa_dantier@hotmail.com>
2023-10-15 21:35:32 -0300

    feat: reduce time in sleep request login

--- src/services/authService/signIn.ts
+++ src/services/authService/signIn.ts
@@ -14,9 +14,9 @@ type SiginInResponse = {
 }
 
 export async function signIn(params: SignInParams) {
-  await sleep()
+  await sleep(1500)
 
-  const { data } = await httpClient.post<SignInPostProps>('/api/login', params)
+  const { data } = await httpClient.post<SignInPostProps>('/api/Login', params)
 
   return data.data;
 }
