24e6caf53eadce7921c1b360bfa5837aad5f6536
larissadantier <larissa_dantier@hotmail.com>
2023-10-18 03:03:30 -0300

    fix: return of isPending

--- src/pages/Occurrences/parts/Table/index.tsx
+++ src/pages/Occurrences/parts/Table/index.tsx
@@ -61,7 +61,7 @@ function TableOcurrence() {
         open={isShowingDelete}
         closeModal={deleteClosed}
         onConfirmModal={() => handleDelete(id)}
-        isLoading={true}
+        isLoading={isPending}
       >
         Tem certeza que deseja excluir a ocorrência Carro bateu na pilastra da garagem da lista de ocorrências do condomínio Ilha de Capri? Esta ação não poderá ser desfeita.
       </Modal>
