6135b48f78bb99ee9976bead9367806f75e9ae3f
larissadantier <larissa_dantier@hotmail.com>
2023-10-18 22:12:13 -0300

    fix: fixiging styles

--- src/pages/LandingPage/styles.ts
+++ src/pages/LandingPage/styles.ts
@@ -1,24 +1,26 @@
 import { styled } from "styled-components";
+import { WIDTH_IMAGE, HEIGHT_IMAGE } from "./stylesUtils";
 
 export const BlueTitleSC = styled.h1`
   color: ${({ theme }) => theme.colors.primary[400]};
   font-size: 3rem;
 `;
 
-
-
 export const MuralSectionSC = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  margin: 4rem 0;
+  
+  margin: ${({ theme }) => theme.spacing[17]} 0;
 `;
 
 export const MuralImageSC = styled.img`
-  margin-top: 3rem;
+  width: ${WIDTH_IMAGE};
+  height: ${HEIGHT_IMAGE};
+
+  margin-top: ${({ theme }) => theme.spacing[13]};
   border-radius: 16px;
-  height: 650px;
-  width: 1150px;
+
   box-shadow: 0px 10px 15px 0px rgba(0, 0, 0, 0.15);
 `;
