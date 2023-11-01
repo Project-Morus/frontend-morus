7dc647b8d007d5c0113c8657ba5d21e6a42dba61
Eduardo Simon <eduardosimon.trabalho@gmail.com>
2023-10-31 23:32:02 -0300

    feat: add graphs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,11 +3,16 @@ import { httpClient } from "../../services/httpClient";
 
 import CardInformations from "./parts/CardInformations";
 import { CardInformationProps } from "./parts/types";
+import { useTheme } from "styled-components";
 import { VictoryArea } from "victory";
+import { GraphContainerSC } from "./styles";
+import { Icon } from "../../components";
 
 const Home = () => {
   const [data, setData] = useState<CardInformationProps[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const theme = useTheme();
 
   useEffect(() => {
     async function fetchData() {
@@ -35,7 +40,56 @@ const Home = () => {
   return (
     <>
       {renderList()}
-      <VictoryArea data={[{ x: 2, y: 35 }]} />
+      <GraphContainerSC>
+        <div>
+          <div style={{ display: "flex", gap: 4 }}>
+            <h3>Consumo de energia</h3>
+            <Icon stroke="fill" name="ph-drop" />
+          </div>
+          <VictoryArea
+            interpolation="natural"
+            style={{
+              data: {
+                fill: theme.colors.primary[100],
+                stroke: theme.colors.primary[500],
+                strokeWidth: 3,
+                strokeLinecap: "round",
+              },
+            }}
+            data={[
+              { x: 1, y: 2 },
+              { x: 2, y: 1 },
+              { x: 3, y: 2 },
+              { x: 4, y: 3 },
+              { x: 5, y: 2 },
+            ]}
+          />
+        </div>
+        <div>
+          <div style={{ display: "flex", gap: 4 }}>
+            <h3>Consumo de energia</h3>
+            <Icon stroke="fill" name="ph-lightning" color={theme.colors.orange[300]} />
+          </div>
+          <VictoryArea
+            interpolation="natural"
+            style={{
+              data: {
+                fill: theme.colors.orange[50],
+                stroke: theme.colors.orange[500],
+                strokeWidth: 3,
+                strokeLinecap: "round",
+              },
+            }}
+            data={[
+              { x: 1, y: 2 },
+              { x: 2, y: 3 },
+              { x: 3, y: 4 },
+              { x: 4, y: 3 },
+              { x: 5, y: 3 },
+            ]}
+          />
+        </div>
+      </GraphContainerSC>
     </>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -4,4 +4,11 @@ export const ContainerSC = styled.div`
   display: flex;
   flex-direction: column;
   gap: ${({ theme }) => theme.spacing[6]};
-`
+`;
+
+export const GraphContainerSC = styled.div`
+  margin-top: ${({ theme }) => theme.spacing[10]};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
